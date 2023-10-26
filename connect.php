0543249f146a2d6dcfd44df1a5fd33f1ef81d9b2
anasada <122490591+anasada@users.noreply.github.com>
2023-10-25 18:54:19 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,11 +8,6 @@ $config=$_POST['config'];
 $point_to=$_POST['signal'];
 $conf_rating=$_POST['conf_rating'];
 
-$dbhost='co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-$dbuser='ciqkb98i04oi5mqj';
-$dbpass='yge7sok0jdptl8eh';
-$db='xj2tpr6deo31cluv';
-$con=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
 
 if($con){
     // echo "connection successful!";
